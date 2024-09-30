adc6b83cb137dfe08b68ae61ce90b7fcc21acc62
Ritesh <riteshkd2k20@gmail.com>
2024-09-30 21:30:07 +0530

    ...

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 
 const app = express();
 const corsOptions = {
-  origin: "https://text-to-speech-client.vercel.app",
+  origin: "https://text-to-speech-client.vercel.app || http://localhost:3000",
   methods: ["POST", "GET"],
   credentials: true,
 };
